aa459aca88681b35ef770f7753f02640fcd2211f
TokyoAnime <150430860+Viewnivers@users.noreply.github.com>
2024-05-30 18:41:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+ 
+
+
+
+// Fonction pour basculer l'état du favori
+function toggleFavorite() {
+    var isFavorite = localStorage.getItem("isFavorite");
+    if (isFavorite && isFavorite === "true") {
+        // Si déjà marqué comme favori, retirez-le des favoris
+        localStorage.setItem("isFavorite", "false");
+    } else {
+        // Si non marqué comme favori, ajoutez-le aux favoris
+        localStorage.setItem("isFavorite", "true");
+    }
+    updateFavoriteButton();
+}
+
+// Fonction pour mettre à jour l'apparence du bouton favori en fonction de son état
+function updateFavoriteButton() {
+    var isFavorite = localStorage.getItem("isFavorite");
+    var favoriteButton = document.querySelector(".favorite-btn");
+    if (isFavorite && isFavorite === "true") {
+        favoriteButton.innerHTML = '<ion-icon name="heart"></ion-icon>'; // Changement de l'icône du bouton en forme de cœur plein
+    } else {
+        favoriteButton.innerHTML = '<ion-icon name="heart-outline"></ion-icon>'; // Changement de l'icône du bouton en forme de cœur vide
+    }
+}
+
+// Appel de la fonction updateFavoriteButton pour mettre à jour l'apparence du bouton lors du chargement de la page
+updateFavoriteButton();
